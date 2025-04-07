a5f547d26b202b699f688b767b656232a03bc574
Valentin Favard <lefav@Host-003.lan>
2025-04-07 21:28:23 +0200

    add the put series exercices and modified the dot h

---
+++ ft_putnbr_fd.c
@@ -0,0 +1,34 @@
+#include "libft.h"
+#include <unistd.h>
+
+void ft_putchar_fd(char c, int fd)
+{
+    write (fd, &c, 1);
+}
+
+void ft_putnbr_fd(int n, int fd)
+{
+    long nb;
+
+    nb = n;
+    if (nb < 0)
+    {
+        nb *= -1;
+        ft_putchar_fd('-', fd);
+    }
+    if (nb >= 10)
+      ft_putnbr_fd(nb / 10, fd);
+    ft_putchar_fd(nb % 10 + '0', fd);
+}
+#include <limits.h>
+
+int main(void)
+{
+    ft_putnbr_fd(INT_MIN, 1);
+    write (1, "\n", 1);
+    ft_putnbr_fd(INT_MAX, 1);
+    write (1, "\n", 1);
+    ft_putnbr_fd(42, 1);
+    write (1, "\n", 1);
+    return 0;
+}
