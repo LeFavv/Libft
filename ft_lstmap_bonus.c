c21513655ae0efc06940f92756393e68ee837299
Valentin Favard <lefav@Host-004.lan>
2025-05-03 16:44:00 +0200

    finished

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -6,7 +6,7 @@
 /*   By: vafavard <vafavard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/28 09:59:10 by vafavard          #+#    #+#             */
-/*   Updated: 2025/04/28 13:50:16 by vafavard         ###   ########.fr       */
+/*   Updated: 2025/05/03 16:43:33 by vafavard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,3 +38,55 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	}
 	return (new_lst);
 }
+/*
+#include <stdio.h>
+
+void *ft_test_f(void *s)
+{
+	return (ft_strdup((char *)s));
+}
+
+void ft_test_del(void *s)
+{
+	free(s);
+}
+
+void ft_dummy_del(void *s)
+{
+	(void)s;
+}
+
+int main(void)
+{
+	t_list *node1 = ft_lstnew("node1");
+	t_list *node2 = ft_lstnew("node2");
+	t_list *node3 = ft_lstnew("node3");
+	t_list *node4 = ft_lstnew("node4");
+
+	node1->next = node2;
+	node2->next = node3;
+	node3->next = node4;
+
+	t_list *test = ft_lstmap(node1, &ft_test_f, &ft_test_del);
+
+	int i = 0;
+	t_list *tmp = test;
+	while (tmp != NULL)
+	{
+		printf("test %d: %s\n", i, (char *)tmp->content);
+		tmp = tmp->next;
+		i++;
+	}
+
+	tmp = node1;
+	i = 0;
+	while (tmp != NULL)
+	{
+		printf("node1 %d: %s\n", i, (char *)tmp->content);
+		tmp = tmp->next;
+		i++;
+	}
+
+	ft_lstclear(&node1, &ft_dummy_del);
+	ft_lstclear(&test, &ft_test_del);
+}*/
